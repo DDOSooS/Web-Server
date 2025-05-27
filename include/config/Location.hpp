b9aecabc317c8a90431043d618a3f98a9f216faf
aezzahir <aero.glowsky@gmail.com>
2025-05-27 17:05:57 +0100

    fixing invalid parameter '' and return

--- include/config/Location.hpp
+++ include/config/Location.hpp
@@ -12,7 +12,7 @@ private:
     bool                        _autoindex;
     std::string                 _index;
     std::vector<bool>          _allow_methods; // GET POST DELETE PUT- HEAD-
-    std::string                 _return;
+    std::vector<std::string>    _return; // 301 /new-page
     std::string                 _alias;
     std::vector<std::string>    _cgi_path;
     std::vector<std::string>    _cgi_ext;
@@ -30,7 +30,7 @@ public:
     void set_autoindex(std::string new_auto_index);
     void set_index(std::string new_index);
     void set_allowMethods(std::vector<std::string> allow_methods); // GET POST- DELETE- PUT- HEAD-
-    void set_return(std::string new_return);
+    void set_return(std::vector<std::string> new_return);
     void set_alias(std::string new_alias);
     void set_cgiPath(std::vector<std::string> cgi_paths);
     void set_cgiExt(std::vector<std::string> cgi_exts);
@@ -40,8 +40,8 @@ public:
     std::string                 get_root_location() const;
     bool                        get_autoindex() const;
     std::string                 get_index() const;
-    std::vector<bool>          get_allowMethods() const; // GET+ POST- DELETE- PUT- HEAD-
-    std::string                 get_return() const;
+    std::vector<bool>           get_allowMethods() const; // GET+ POST- DELETE- PUT- HEAD-
+    std::vector<std::string>    get_return() const;
     std::string                 get_alias() const;
     std::vector<std::string>    get_cgiPath() const;
     std::vector<std::string>    get_cgiExt() const;
