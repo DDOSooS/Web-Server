80697070dec10f9086f2617188de428e6321a375
DDOSooS <abde.gherofficiel@gmail.com>
2025-06-16 17:41:16 +0100

    fixing sig pipe error

--- include/error/ErrorHandler.hpp
+++ include/error/ErrorHandler.hpp
@@ -16,7 +16,6 @@ class ErrorHandler
         virtual const char*     what() const throw();
         virtual bool            CanHandle(ERROR_TYPE ) const = 0;
         virtual void            ProcessError(Error &error, const ServerConfig & /* server Configuration*/) = 0;
-        virtual void            DefaultErrorHandler(Error &error);
         bool                    IsErrorPageDefined(const ServerConfig &config, short error_code) const;
         void                    ErrorPageChecker(Error &error, const ServerConfig &config);
         virtual ~ErrorHandler();
---
+++ www/cgi-bin/inif.py
@@ -0,0 +1,10 @@
+import sys
+
+#!/usr/bin/env python3
+
+
+print("Content-Type: text/plain\n")
+
+while True:
+    print("This is an infinite loop.")
+    sys.stdout.flush()
