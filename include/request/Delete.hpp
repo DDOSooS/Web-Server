348e139b14cf2fc1dd9f30fd1f23a6c64cca8814
aezzahir <aero.glowsky@gmail.com>
2025-06-29 15:29:08 +0100

    Initial setup of delete ... delete a regular file

--- include/request/Delete.hpp
+++ include/request/Delete.hpp
@@ -1,27 +1,21 @@
 #ifndef DELETE_HPP
 #define DELETE_HPP
 
-#include "./RequestHandler.hpp"
-#include <string>
-#include <map>
+#include "RequestHandler.hpp"
+#include "HttpRequest.hpp"
+#include "Get.hpp"
+#include "../config/ServerConfig.hpp"
 
-class Delete : public RequestHandler {
-private:
-    std::string htmlEscape(const std::string &input);
-    void setErrorResponse(HttpRequest *request, int statusCode, const std::string &statusMessage, const std::string &message);
-    bool isPathSafe(const std::string &path);
-    bool deleteFile(const std::string &filePath);
-    bool deleteDirectory(const std::string &dirPath);
-    std::string getUploadsDirectory();
-    std::map<std::string, std::string> parseQueryString(const std::string &queryString);
-    std::string urlDecode(const std::string &encoded);
-    bool isDirectoryEmpty(const std::string &dirPath);
-
+class Delete : public Get {
 public:
     Delete();
     ~Delete();
+    
+    // Inherited from RequestHandler
     bool CanHandle(std::string method);
     void ProccessRequest(HttpRequest *request, const ServerConfig &serverConfig, ServerConfig clientConfig);
+    void sendSuccessResponse(HttpRequest *request);
+    void sendErrorResponse(HttpRequest *request, int statusCode, const std::string &statusMessage, const std::string &errorMessage);
 };
 
-#endif
+#endif // DELETE_HPP
